e6455eac2105b89b7b3cfe5a8be23160c13c832d
theNerd247 <noah.harvey247 gm>
2017-09-04 12:48:00 -0400

    adds lenses

--- app/Main.hs
+++ app/Main.hs
@@ -1,16 +1,19 @@
 {-# LANGUAGE DeriveDataTypeable #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
 
 module Main where
 
+import Control.Lens
 import Control.Exception
 import Control.Monad
 import Data.Data
+import Data.Default
 import Data.Monoid
 import qualified Data.Text as DT
 import Data.Time
-import Data.Yaml
+import Data.Yaml hiding ((.~))
 import GHC.Generics
 import qualified Data.List as DL
 
@@ -23,25 +26,49 @@ data BudgetType =
   deriving (Eq,Ord,Show,Read,Data,Typeable,Generic)
 
 data BudgetItem = BudgetItem 
-  { amount :: Amount
-  , rate :: Rate
-  , budgetType :: BudgetType
+  { _budgetType :: BudgetType
+  , _rate :: Rate
+  , _amount :: Amount
   } deriving (Eq,Ord,Show,Read,Data,Typeable,Generic)
 
-type Budget = [BudgetItem]
+data Budget = Budget 
+  { _name :: String
+  , _startDate :: Day
+  , _startAmount :: Amount
+  , _items :: [BudgetItem]
+  } deriving (Eq,Ord,Show,Read,Data,Typeable,Generic)
+
+makeLenses ''Budget
 
 data Bank = Bank
-  { checking :: Amount
-  , savings :: Amount
-  , lastModified :: Rate
+  { _checking :: Amount
+  , _savings :: Amount
+  , _lastModified :: Rate
   } deriving (Eq,Ord,Show,Read,Data,Typeable,Generic)
 
-instance ToJSON BudgetType where
-  toJSON Income = String $ DT.pack "income"
-  toJSON (Expense s) = String $ DT.pack s
+makeLenses ''Bank
 
-instance ToJSON BudgetItem
-instance ToJSON Bank
+instance Default BudgetItem where
+  def = BudgetItem
+    { _amount = 0
+    , _rate = 0
+    , _budgetType = Income
+    }
+
+instance Default Budget where
+  def = Budget
+    { _items = []
+    , _name = ""
+    , _startDate = fromGregorian 0 0 0
+    , _startAmount = 0
+    }
+
+instance Default Bank where
+  def = Bank
+    { _checking = 0
+    , _savings = 0
+    , _lastModified = 0
+    }
 
 instance FromJSON BudgetType where
   parseJSON (String s)
@@ -50,116 +77,159 @@ instance FromJSON BudgetType where
   parseJSON _ = fail "Budget type is the wrong yaml type - should be a string"
 
 instance FromJSON BudgetItem
+
+instance FromJSON Budget
+
 instance FromJSON Bank
 
-toExpensesOnly :: Budget -> Budget
-toExpensesOnly = filter (noIncome . budgetType)
+instance ToJSON BudgetType where
+  toJSON Income = String $ DT.pack "income"
+  toJSON (Expense s) = String $ DT.pack s
+
+instance ToJSON BudgetItem
+
+instance ToJSON Budget
+
+instance ToJSON Bank
+
+amount :: Lens' BudgetItem Amount
+amount = lens gt st
   where
-    noIncome Income = False
-    noIncome (Expense _) = True
+    st s a = BudgetItem 
+      { _amount = -1*(abs a)
+      , _rate = _rate s
+      , _budgetType = _budgetType s
+      }
+    gt = _amount
+
+rate :: Lens' BudgetItem Rate
+rate = lens gt st
+  where
+    st s r = BudgetItem 
+      { _amount = _amount s
+      , _rate = r
+      , _budgetType = _budgetType s
+      }
+    gt = _rate
+
+budgetType :: Lens' BudgetItem BudgetType
+budgetType = lens gt st
+  where
+    st s t = BudgetItem 
+      { _amount = _amount s
+      , _rate = _rate s
+      , _budgetType = t
+      }
+    gt = _budgetType
+
 
 getExpense :: String -> Budget -> Maybe BudgetItem
-getExpense n = DL.find (isName . budgetType)
+getExpense n b = DL.find (isName . _budgetType) $ b^.items
   where
     isName Income = False
     isName (Expense t) = n == t
 
-payOffAfter :: String -> Amount -> Rate -> Budget -> Maybe Amount
-payOffAfter ename a r b = do
-  e <- getExpense ename b
-  return $ getBalanceAtPeriod a r [e]
-
 -- runs a budget for "p" periods given a starting amount "start" and returns the
 -- final balance. 
-getBalanceAtPeriod :: Amount -> Rate -> Budget -> Amount
-getBalanceAtPeriod start p = (start +) . getSum . mconcat . fmap 
-  (\i -> Sum $ (amount i)*(fromIntegral . floor . toRational $ p `div` (rate i)))
+getBalanceAtPeriod :: Rate -> Budget -> Amount
+getBalanceAtPeriod p b = (b^.startAmount +) . getSum . mconcat . fmap sumItem $ b^.items
+  where
+    sumItem i = Sum $ (i^.amount)*(fromIntegral . floor . toRational $ p `div` (i^.rate))
 
-printBalances :: Amount -> Rate -> Budget -> IO ()
-printBalances start ds budget = sequence_ $ [printBal d | d <- [0..ds]]
+printBalances :: Rate -> Budget -> IO ()
+printBalances ds budget = sequence_ $ [printBal d | d <- [0..ds]]
   where
     printBal d = putStrLn $ 
          (show d)
       ++ ": "
-      ++ (show $ getBalanceAtPeriod start d budget)
+      ++ (show $ getBalanceAtPeriod d budget)
 
-getEmptyDate :: Double -> Budget -> Int
-getEmptyDate start budget = g 0
+getEmptyDate :: Budget -> Int
+getEmptyDate budget = g 0
     where
       g n
         | (f n) <= 0 = n
         | otherwise = g (n+1)
-      f n = getBalanceAtPeriod start (n+1) budget
+      f n = getBalanceAtPeriod (n+1) budget
 
-makeExpense :: String -> Amount -> Rate -> BudgetItem
-makeExpense t a r = BudgetItem 
-  { amount = (-1)*(abs a)
-  , rate = r
-  , budgetType = Expense t
-  }
+newIncome :: BudgetItem
+newIncome = def
 
-makeIncome :: Amount -> Rate -> BudgetItem
-makeIncome a r = BudgetItem
-  { amount = abs a
-  , rate = r
-  , budgetType = Income
-  }
+newExpense :: String -> BudgetItem
+newExpense s = def & budgetType .~ (Expense s)
 
-startDate = fromGregorian 2017 09 01
+currentDay = utctDay <$> getCurrentTime
 
-dayToRate :: Day -> Rate
-dayToRate = fromInteger . flip diffDays startDate  
+dayToRate :: Day -> Day -> Rate
+dayToRate s = fromInteger . flip diffDays s
 
-rateToDay :: Rate -> Day
-rateToDay = flip addDays startDate . toInteger
+rateToDay :: Day -> Rate -> Day
+rateToDay s = flip addDays s . toInteger
 
 loadBudgetsFile :: FilePath -> IO [Budget]
 loadBudgetsFile fp = decodeFileEither fp >>= either throwIO return
 
+currentBudgetBal :: Budget -> IO Amount
+currentBudgetBal b = do 
+  n <- currentDay 
+  return $ getBalanceAtPeriod (dayToRate (b^.startDate) n) b
+
 income = 1730.77 :: Amount
 loanAmount = 11352.14 :: Amount
 
-bank = Bank
-  { checking = 3868.88
-  , savings = 1848.55
-  , lastModified = 0
-  }
+bank = def & checking .~ 3868.88 & savings .~ 1848.55
 
-budgetLoan = [ makeExpense "loan" (income - (income*0.1 + income*0.2)) 15 ]
+budgetLoan = def 
+  & name .~ "Loan"
+  & startDate .~ fromGregorian 2017 09 01
+  & startAmount .~ loanAmount
+  & items .~ 
+    [newExpense "loan" 
+      & amount .~ (income - (income*0.1 + income*0.2)) 
+      & rate .~ 15 
+    ]
 
-budgetLiving =
-  [ makeIncome 2000 500
-  , makeIncome (income*0.1 + income*0.2) 15
-  , makeExpense "tithe" (income*0.1) 15
-  , makeExpense "taxes" (income*0.2) 15
-  , makeExpense "rent" 350 31
-  , makeExpense "auto" 20 7
-  , makeExpense "food" 20 7
-  , makeExpense "insurance" 76 31
-  , makeExpense "phone" 30 31 
-  , makeExpense "climbing" 55 31
-  , makeExpense "mentoring" 7 31
-  ]
+budgetLiving = def
+  & name .~ "Living"
+  & startDate .~ fromGregorian 2017 09 01
+  & startAmount .~ income
+  & items .~ 
+    [ newIncome & amount .~ 2000 & rate .~ 500
+    , newIncome & amount .~ (income*0.1 + income*0.2) & rate .~ 15
+    , newExpense "tithe" & amount .~ (income*0.1) & rate .~ 15
+    , newExpense "taxes" & amount .~ (income*0.2) & rate .~ 15
+    , newExpense "rent" & amount .~ 350 & rate .~  31
+    , newExpense "auto" & amount .~  20 & rate .~ 7
+    , newExpense "food" & amount .~ 20 & rate .~ 7
+    , newExpense "insurance" & amount .~ 76 & rate .~ 31
+    , newExpense "phone" & amount .~ 30 & rate .~ 31 
+    , newExpense "climbing" & amount .~ 55 & rate .~ 31
+    , newExpense "mentoring" & amount .~ 7 & rate .~ 31
+    ]
 
 main :: IO ()
 main = do 
   bs <- loadBudgetsFile "budgets.yaml"
-  now <- utctDay <$> getCurrentTime
+  now <- currentDay
   let
     budgetLoan = bs !! 0
-    budgetLiving = bs !! 1
-    n = getEmptyDate (loanAmount) budgetLoan
-    m = getEmptyDate (checking bank) budgetLiving
-    x n = getBalanceAtPeriod (checking bank) n budgetLiving
-    y n = getBalanceAtPeriod (loanAmount) n budgetLoan
-  putStrLn $ "Loan can be payed off in: " ++ (show n) ++ " days - " ++ (show . rateToDay $ n) 
-  putStrLn $ "Your living budget will last: " ++ (show m) ++ " days - " ++ (show . rateToDay $ m) 
-  putStrLn $ "The amount in your bank will be: " ++ (show $ x n)
+    budgetLiving = (bs !! 1) & startAmount %~ (\x -> x-600)
+    n = getEmptyDate budgetLoan
+    x = getBalanceAtPeriod (n+1) budgetLoan
+    m = getEmptyDate budgetLiving
+    y = getBalanceAtPeriod (n+1) budgetLiving
+  putStrLn $ "Loan can be payed off in: " 
+    ++ (show n) 
+    ++ " days - " 
+    ++ (show . rateToDay (budgetLoan^.startDate) $ n) 
+    ++ " - " ++ (show x)
+  putStrLn $ "Your living budget will last: " 
+    ++ (show m) 
+    ++ " days - " 
+    ++ (show . rateToDay (budgetLiving^.startDate) $ m) 
+    ++ " - after loan: " ++ (show y)
+  lo <- currentBudgetBal budgetLoan
+  li <- currentBudgetBal budgetLiving
   putStrLn $ "Current balances should be: "
-  putStrLn $ "  Living: " ++ (show . x $ dayToRate now)
-  putStrLn $ "  Loan: " ++ (show . y $ dayToRate now)
-  {-putStrLn $ "You can pay off " ++ (show x) ++ " of your loan"-}
-  {-putStrLn $ "Loan Budget lasts " ++ (show $ getBalanceAtPeriod loanAmount 122 budgetLoan) ++ " days"-}
-  {-printBalances loanAmount 122 budgetLoan-}
-  {-putStrLn $ "Diff: "  ++ (show x)-}
-  where
+  putStrLn $ "  Living: " ++ (show $ lo)
+  putStrLn $ "  Loan: " ++ (show $ li)
