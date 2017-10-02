b6359205c60b023a098f6d23928b6d9aaa58064a
theNerd247 <theNerd247@users.noreply.github.com>
2017-10-01 20:45:16 -0400

    Fixes expenses db 2,7 (#11)
    * adds acid-state and safecopy to project
    
    * adds acid state repo and some auto-merging of expenses
    
    * adds expenses database
    
    * adds ability to force merge expenses files
    
    * adds some bug fixes
    
    * adds expenses for august and partway through september
    
    * changes db to contain only a test Expenses
    
    * adds bug fix with backing up files
    
    * adds db updates

--- app/Main2.hs
+++ app/Main2.hs
@@ -1,9 +1,16 @@
 module Main where
 
+import Data.Acid
+import Data.Yaml (encodeFile)
 import Data.Budget
+import Control.Exception
+import Control.Monad.Catch
 import Data.Default (def)
-import Control.Lens
+import Control.Lens hiding ((<.>))
 import Data.Time (utctDay, getCurrentTime, fromGregorian)
+import System.Directory
+import System.FilePath
+import Data.Traversable (forM)
 
 income = 1730.77 :: Amount
 loanAmount = 11352.14 :: Amount
@@ -43,5 +50,57 @@ budgetLiving = def
     , mkBItem "climbing"  55 31
     , mkBItem "mentoring" 7  31
     ]
-    
-main = currentBudgetBal budgetLoan >>= putStrLn . show 
+
+getFromDir d ext = listDirectory d >>= return . fmap (d </>). filter ((==ext) . takeExtension)
+
+data NoNameException = NoNameException String
+  deriving (Eq,Ord,Show,Read)
+
+instance Exception NoNameException
+
+guardNoName :: FilePath -> Expenses -> IO a -> IO a
+guardNoName f e m
+  | e^.name == "" = throwM . NoNameException $ "You need to name the expenses! Not merging: " ++ f
+  | otherwise = m
+
+prefixOf n = reverse . take n . reverse
+
+backupFile :: FilePath -> IO ()
+backupFile f = renameFile f (f <.> ".bak")
+
+main = do
+  -- open database
+  db <- openLocalStateFrom "expenses-acid" ([] :: ExpensesDB)
+  -- search for new transaction files
+  newTransactionFiles <- getFromDir "transactions" ".csv"
+  -- search for new expenses files
+  newExpensesFiles <- getFromDir "transactions" ".yaml" >>= return . filter ((\x -> x/="merge.yaml" && x/="_dups.yaml") . prefixOf 10)
+  -- convert and save the new transactions so we can add reasons
+  forM newTransactionFiles $ \f -> do 
+    loadNewTransactionFile "" f
+    backupFile f
+    putStrLn $ "Expenses file ready! " ++ (f -<.> "yaml")
+  -- force insert merged transactions
+  forceMergeFiles <- getFromDir "transactions" ".yaml" >>= return . filter ((=="_merge.yaml") . prefixOf 11)
+  forM forceMergeFiles $ \f -> do
+    e <- loadYamlFile f
+    guardNoName f e $ do
+      update db . InsertExpenses $ e
+      putStrLn $ "Force merged expenses in: " ++ f
+      backupFile f
+  -- upsert new expenses files
+  forM newExpensesFiles $ \f -> do 
+    e <- loadYamlFile f :: IO Expenses
+    guardNoName f e $ do
+      dups <- update db . UpsertExpenses $ e
+      case dups of
+        [] -> do 
+          putStrLn $ f ++ " Successfully loaded!"
+        _ -> do
+          putStrLn $ "You have duplicate expenses in: " ++ f
+          putStrLn $ "I've merged the unique entries for you...you'll find the duplicates in: " ++ dupsFP
+          putStrLn $ "Edit this file and rename _dups.yaml to _merge.yaml to force merge the expenses: " ++ dupsFP
+          encodeFile dupsFP (e & items .~ dups)
+          where
+            dupsFP = ((f -<.> "") ++ "_dups.yaml")    
+    backupFile f
