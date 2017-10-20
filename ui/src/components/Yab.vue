d864a1ed16331d59ba5a34dc03b911d975b1d0d5
theNerd247 <noah.harvey247 gm>
2017-10-19 21:43:49 -0400

    refactored other cards of home page

--- ui/src/components/Yab.vue
+++ ui/src/components/Yab.vue
@@ -21,60 +21,27 @@
 			</el-col>
 
       <el-col :span="8">
-				<home-card title="Add Expense"> 
-					<el-button slot="action" @click="createNewExpense" type="info" style="float: right;"><i class="el-icon-plus"></i> Add</el-button> 
-					<el-form slot="content"> 
-						<el-form-item> 
-							<el-input v-model="newExpense.name" placeholder="Budget Name"></el-input>
-						</el-form-item>
-						<el-form-item>
-								<el-input v-model="newExpense.reason" placeholder="Reason"></el-input>
-						</el-form-item>
-						<el-form-item>
-								<el-input v-model="newExpense.amount" placeholder="Amount">
-								<span slot="prepend">$</span>
-								</el-input>
-						</el-form-item>
-						</el-form>
-						</home-card>
+				<el-row>
+					<add-expense></add-expense>
+				</el-row>
+				<el-row>
+					<upload-transaction></upload-transaction>
+				</el-row>
 			</el-col>
+
 			<el-col :span="8">
-				<home-card title="Upload Transactions">
-					<el-button slot="action" @click="uploadTransaction" type="info" style="float: right;"><i class="el-icon-upload"></i> Upload</el-button>
-					<el-form slot="content">
-					<el-form-item>
-						<el-input placeholder="Budget Name"></el-input>
-					</el-form-item>
-					<el-form-item>
-						<el-col :span="16" :offset="4">
-							<el-upload class="upload-demo" drag action="https://jsonplaceholder.typicode.com/posts/" multiple>
-								<i class="el-icon-upload"></i>
-									<div class="el-upload__text">Drop file here or <em>click to upload</em></div>
-								</el-upload>
-							</el-col>
-						</el-form-item>
-          </el-form>
-        </home-card>
+				<status-chart></status-chart>
       </el-col>
     </el-row>
+
     <el-row :gutter="20" justify="space-around">
       <el-col :span="8">
-        <home-card title="Current Balances">
-            <el-table slot="content" :data="budgets" :row-class-name="budgetStatus">
-              <el-table-column prop="name" label="Budget" ></el-table-column>
-              <el-table-column prop="amount" label="Amount" > </el-table-column>
-              <el-table-column prop="limit" label="Limit"> </el-table-column>
-          </el-table>
-        </home-card>
       </el-col>
 
       <el-col :span="8">
-        <home-card title="Recent Expenses">
-        </home-card>
       </el-col>
 
       <el-col :span="8">
-        <home-card></home-card>
       </el-col>
     </el-row>
 
@@ -85,33 +52,24 @@
 <script>
 import Vue from 'vue'
 import BudgetGraph from '@/components/home/BudgetsGraph.vue'
-import budgetsJSON from '../assets/budgets.json'
+import StatusChart from '@/components/home/StatusChart.vue'
+import UploadTransaction from '@/components/home/UploadTransaction.vue'
+import AddExpense from '@/components/home/AddExpense.vue'
 import HomeCard from '@/shared/HomeCard.vue'
 
 export default {
   components: {
     HomeCard,
-		BudgetGraph
+		BudgetGraph,
+		StatusChart,
+		UploadTransaction,
+		AddExpense,
   },
   name: 'Vue',
   data () {
     return {
-      budgets: budgetsJSON,
-      newExpense: {
-        name: '',
-        amount: null,
-        reason: ''
-      },
     }
   },
-  methods: {
-    budgetStatus(budget) {
-      if(budget.amount > budget.limit)
-        return 'alert-row';
-      else
-        return '';
-    }
-  }
 }
 </script>
 
---
+++ ui/src/components/home/StatusChart.vue
@@ -0,0 +1,34 @@
+<template>
+<home-card title="Current Balances">
+		<el-table slot="content" :data="budgets" :row-class-name="budgetStatus">
+			<el-table-column prop="name" label="Budget" ></el-table-column>
+			<el-table-column prop="amount" label="Amount" > </el-table-column>
+			<el-table-column prop="limit" label="Limit"> </el-table-column>
+	</el-table>
+</home-card>
+</template>
+
+<script>
+import Vue from 'vue'
+import HomeCard from '@/shared/HomeCard.vue'
+import budgetsJSON from '@/assets/budgets.json'
+
+export default {
+  components: {
+    HomeCard,
+	},
+	data  () {
+		return {
+      budgets: budgetsJSON,
+		}
+	},
+  methods: {
+    budgetStatus(budget) {
+      if(budget.amount > budget.limit)
+        return 'alert-row';
+      else
+        return '';
+    }
+  }
+}
+</script>
