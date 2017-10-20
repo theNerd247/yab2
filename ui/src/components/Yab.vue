a0413f1d7f3dc3d983d25c1e6fc098e0f7b52f95
theNerd247 <noah.harvey247 gm>
2017-10-19 21:21:25 -0400

    refactored home page budget graph chart

--- ui/src/components/Yab.vue
+++ ui/src/components/Yab.vue
@@ -15,22 +15,49 @@
       </el-menu>
     </el-row>
 
-    <el-row :gutter="20">
+    <el-row type="flex" justify="space-around" :gutter="20">
       <el-col :span="8">
-        <home-card title="Budget Status">
-          <div slot="content">
-            <balances-chart ref="bchart" :chart-data="balancesData"></balances-chart>
-          </div>
-        </home-card>
-      </el-col>
+				<budget-graph></budget-graph>
+			</el-col>
 
       <el-col :span="8">
-        <home-card title="Add Expense"> <el-button slot="action" @click="createNewExpense" style="float: right;">New</el-button> <el-form slot="content"> <el-form-item> <el-input v-model="newExpense.name" placeholder="Budget Name"></el-input> </el-form-item> <el-form-item> <el-input v-model="newExpense.reason" placeholder="Reason"></el-input> </el-form-item> <el-form-item> <el-input v-model="newExpense.amount" placeholder="Amount"> <span slot="prepend">$</span> </el-input> </el-form-item> </el-form> </home-card> </el-col> <el-col :span="8"> <home-card title="Upload Transactions"> <el-button slot="action" @click="uploadTransaction" style="float: right;">Upload</el-button> <el-form slot="content"> <el-form-item> <el-input placeholder="Budget Name"></el-input> </el-form-item> <el-form-item> <el-upload class="upload-demo" drag action="https://jsonplaceholder.typicode.com/posts/" multiple> <i class="el-icon-upload"></i> <div class="el-upload__text">Drop file here or <em>click to upload</em></div> </el-upload> </el-form-item>
+				<home-card title="Add Expense"> 
+					<el-button slot="action" @click="createNewExpense" type="info" style="float: right;"><i class="el-icon-plus"></i> Add</el-button> 
+					<el-form slot="content"> 
+						<el-form-item> 
+							<el-input v-model="newExpense.name" placeholder="Budget Name"></el-input>
+						</el-form-item>
+						<el-form-item>
+								<el-input v-model="newExpense.reason" placeholder="Reason"></el-input>
+						</el-form-item>
+						<el-form-item>
+								<el-input v-model="newExpense.amount" placeholder="Amount">
+								<span slot="prepend">$</span>
+								</el-input>
+						</el-form-item>
+						</el-form>
+						</home-card>
+			</el-col>
+			<el-col :span="8">
+				<home-card title="Upload Transactions">
+					<el-button slot="action" @click="uploadTransaction" type="info" style="float: right;"><i class="el-icon-upload"></i> Upload</el-button>
+					<el-form slot="content">
+					<el-form-item>
+						<el-input placeholder="Budget Name"></el-input>
+					</el-form-item>
+					<el-form-item>
+						<el-col :span="16" :offset="4">
+							<el-upload class="upload-demo" drag action="https://jsonplaceholder.typicode.com/posts/" multiple>
+								<i class="el-icon-upload"></i>
+									<div class="el-upload__text">Drop file here or <em>click to upload</em></div>
+								</el-upload>
+							</el-col>
+						</el-form-item>
           </el-form>
         </home-card>
       </el-col>
     </el-row>
-    <el-row :gutter="20">
+    <el-row :gutter="20" justify="space-around">
       <el-col :span="8">
         <home-card title="Current Balances">
             <el-table slot="content" :data="budgets" :row-class-name="budgetStatus">
@@ -57,14 +84,14 @@
 
 <script>
 import Vue from 'vue'
-import HomeCard from './HomeCard.vue'
+import BudgetGraph from '@/components/home/BudgetsGraph.vue'
 import budgetsJSON from '../assets/budgets.json'
-import BalancesChart from '../shared/BalancesChart.vue'
+import HomeCard from '@/shared/HomeCard.vue'
 
 export default {
   components: {
     HomeCard,
-    BalancesChart
+		BudgetGraph
   },
   name: 'Vue',
   data () {
@@ -75,50 +102,8 @@ export default {
         amount: null,
         reason: ''
       },
-			balancesData: null,
-      gradient1: null,
-			gradient2: null
     }
   },
-  mounted () {
-		let ys = [];
-		let xs = [];
-		let is = [];
-		for (var i = 0; i <= 62.8; i++){
-			is[i] = i;
-			xs[i] = 0.2*Math.cos(0.1*i);
-			ys[i] = Math.sin(0.1*i);
-		};
-
-		this.gradient1 =
-			this.$refs.bchart.$refs.canvas.getContext('2d').createLinearGradient(0, 0, 0, 1000);
-		this.gradient1.addColorStop(0, 'rgba(19, 206, 102, 0.5)')
-		this.gradient1.addColorStop(0.5, 'rgba(19, 206, 102, 0.25)');
-		this.gradient1.addColorStop(1, 'rgba(19, 206, 102, 0)');
-
-		this.gradient2 =
-			this.$refs.bchart.$refs.canvas.getContext('2d').createLinearGradient(0, 0, 0, 1000);
-		this.gradient2.addColorStop(0, 'rgba(32, 160,255, 1)')
-		this.gradient2.addColorStop(0.5, 'rgba(32, 160, 255, 0)');
-		this.gradient2.addColorStop(1, 'rgba(32, 160, 255, 1)');
-
-
-		this.balancesData = {
-			labels: is,
-			datasets: [
-				{
-					label: 'Budget',
-					backgroundColor: this.gradient2,
-					data: ys
-				},
-				{
-					label: 'Expenses',
-					backgroundColor: this.gradient1,
-					data: xs 
-				}
-			]
-		};    
-	},
   methods: {
     budgetStatus(budget) {
       if(budget.amount > budget.limit)
