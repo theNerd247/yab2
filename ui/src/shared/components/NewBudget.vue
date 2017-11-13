ce26df6864437383ee72f1e3660016c1a569fca5
theNerd247 <noah.harvey247 gm>
2017-11-13 16:05:50 -0500

    adds custom theme

--- ui/src/shared/components/NewBudget.vue
+++ ui/src/shared/components/NewBudget.vue
@@ -8,21 +8,21 @@
 			<el-form-item>
 				<el-input v-model.number="newBudget.startInfo.startAmount" type="number" placeholder="Start Amount"></el-input>
 			</el-form-item>
-      <el-form-item>
-           <el-date-picker
-            v-model="newBudget.startInfo.startDate"
-            type="date"
-            format="yyyy-MM-dd"
-            placeholder="Budget Start Day">
-          </el-date-picker>
-      </el-form-item>
-      <el-form-item>
-          <el-button @click="addItem()"><i class="el-icon-plus"></i> Add Item</el-button>
-      </el-form-item>
+			<el-form-item>
+				<el-date-picker
+					v-model="newBudget.startInfo.startDate"
+					type="date"
+					format="yyyy-MM-dd"
+					placeholder="Budget Start Day">
+				</el-date-picker>
+			</el-form-item>
+			<el-form-item>
+				<el-button @click="addItem()"><i class="el-icon-plus"></i> Add Item</el-button>
+			</el-form-item>
 			<el-form-item v-for="(item, index) in newBudget.items" :key="index">
-        <el-input v-model="item.type" placeholder="Item Type">
-          <el-button slot="prepend" @click="removeItem(index)"><i class="el-icon-circle-cross"></i></el-button>
-        </el-input>
+				<el-input v-model="item.type" placeholder="Item Type">
+					<el-button slot="prepend" @click="removeItem(index)"><i class="el-icon-circle-cross"></i></el-button>
+				</el-input>
 				<el-input v-model.number="item.amount" type="number" placeholder="Item Amount"></el-input>
 				<el-input v-model.number="item.rate" type="number" placeholder="Item Rate"></el-input>
 			</el-form-item>
@@ -31,11 +31,11 @@
 </template>
 
 <script>
-import Vue from 'vue'
+	import Vue from 'vue'
 import HomeCard from './HomeCard.vue'
 import budgetsJSON from '@/assets/budgets.json'
 import _ from 'lodash'
-import { HTTP, baseURL } from '@/shared/http-common'
+import { HTTP, httpWithNotify } from '@/shared/http-common'
 import moment from 'moment'
 
 export default {
@@ -45,41 +45,41 @@ export default {
 	data  () {
 		return {
 			newBudget: {
-        startInfo: {
-          name: '',
-          id: '',
-          startAmount: 0,
-          startDate: moment(),
-        },
-        items: []
+				startInfo: {
+					name: '',
+					id: '',
+					startAmount: 0,
+					startDate: moment(),
+				},
+				items: []
 			}
 		}
 	},
 	methods: {
-    createNewBudget () { 
-      _.remove(this.newBudget.items, x => x.amount === null || x.rate == null || x.rate < 0)
+		createNewBudget () {
+			_.remove(this.newBudget.items, x => x.amount === null || x.rate == null || x.rate < 0)
 
-      this.newBudget.startInfo.startDate = moment(this.newBudget.startDate).format();
-      this.newBudget.items = _.map(this.newBudget.items, x => {x.name = this.newBudget.name; return x});
+			this.newBudget.startInfo.startDate = moment(this.newBudget.startDate).format();
+			this.newBudget.items = _.map(this.newBudget.items, x => {x.name = this.newBudget.name; return x});
 
-      httpWithNotify(
-        'Created new budget', 
-        'Could not create budget',
-        function () { return HTTP.post('budget-list', this.newBudget) });
-      },
-
-    removeItem(index) {
-      this.newBudget.items.splice(index,1);
-    },
-    addItem(){
-      this.newBudget.items.push( {
-        id: 0, 
-        type: '', 
-        amount: null, 
-        rate: {tag: "Periodic", contents: 0}, 
-        name: '' 
-      });
-    }
+			httpWithNotify(
+				'Created new budget',
+				'Could not create budget',
+				HTTP.post('budget-list', this.newBudget)
+			);
+		},
+		removeItem(index) {
+			this.newBudget.items.splice(index,1);
+		},
+		addItem(){
+			this.newBudget.items.push( {
+				id: 0,
+				type: '',
+				amount: null,
+				rate: {tag: "Periodic", contents: 0},
+				name: ''
+			});
+		}
 	}
 }
 </script>
