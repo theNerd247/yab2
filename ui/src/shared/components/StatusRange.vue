16d47973f24a586c060420cd2d5d13f9b7e7ba9d
theNerd247 <noah.harvey247 gm>
2017-11-14 17:00:02 -0500

    moved status info above bar

--- ui/src/shared/components/StatusRange.vue
+++ ui/src/shared/components/StatusRange.vue
@@ -3,7 +3,7 @@
   .lowBudget .el-slider__button {
     border-color: #FA5555 !important;
   }
-  
+
   .lowBudget .el-slider__bar {
     background-color: #FA5555 !important;
   }
@@ -11,28 +11,30 @@
   .highBudget .el-slider__button {
     border-color: #67C23A !important;
   }
-  
+
   .highBudget .el-slider__bar {
     background-color: #67C23A !important;
   }
-
 
 </style>
 <template>
   <el-row>
     <h3>Status</h3>
     <el-row>
+      <el-col :span="8">
+        Budget: ${{ status[0] }}
+      </el-col>
+      <el-col :span="8">
+        Available: ${{ status[1] - status[0] }}
+      </el-col>
+      <el-col :span="8">
+        Current: ${{ status[1] }}
+      </el-col>
+    </el-row>
+    <el-row>
       <el-col>
         <el-slider disabled :class="{lowBudget: isLow, highBudget: !isLow}" v-model="stat" :max="statMax" :min="statMin" range :format-tooltip="format"></el-slider>
       </el-col>
-      <el-row>
-        <el-col :span="12">
-          Budget: ${{ status[0] }}
-        </el-col>
-        <el-col :span="12">
-          Current: ${{ status[1] }}
-        </el-col>
-      </el-row>
     </el-row>
   </el-row>
 </template>
